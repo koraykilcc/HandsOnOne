d8034605be0bfef5dcde73b9fcdf5aacf1a3ea0e
Rahmi Kılıç <koray.kilic@metu.edu.tr>
2023-05-28 17:22:38 +0300

    feature

--- src/main/java/tr/metu/sm703/HomeController.java
+++ src/main/java/tr/metu/sm703/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
     @Get
     public Map<String, Object> index() {
 
-        return Collections.singletonMap("message", "Hello World bugfix!");
+        return Collections.singletonMap("message", "Hello World feature!");
 
     }
 }
