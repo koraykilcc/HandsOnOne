7a281b58cd0b71f4583a7086dbe38b14a43b79f4
Rahmi Kılıç <koray.kilic@metu.edu.tr>
2023-05-30 16:26:27 +0300

    dfsd

--- src/main/java/tr/metu/sm703/HomeController.java
+++ src/main/java/tr/metu/sm703/HomeController.java
@@ -3,14 +3,23 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import java.util.Collections;
 import java.util.Map;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Controller
 public class HomeController {
 
     @Get
     public Map<String, Object> index() {
-        String username="koray";
-        System.out.println("<h1>Welcome "+username+"</h1>");
+        String input1 = "password1";
+        String input2 = "password2";
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] hash1 = md.digest(input1.getBytes());
+        byte[] hash2 = md.digest(input2.getBytes());
+
+        System.out.println("Hash1: " + new String(hash1));
+        System.out.println("Hash2: " + new String(hash2));
         return Collections.singletonMap("message", "Hello World feature!");//as
 
     }
