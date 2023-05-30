8799e8eb258c73cbdbc76b8ea5e9aacb03892d4c
Rahmi Kılıç <koray.kilic@metu.edu.tr>
2023-05-30 16:35:55 +0300

    sddf

--- src/main/java/tr/metu/sm703/HomeController.java
+++ src/main/java/tr/metu/sm703/HomeController.java
@@ -1,25 +1,22 @@
 package tr.metu.sm703;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Collections;
 import java.util.Map;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+
+
 
 @Controller
 public class HomeController {
 
     @Get
     public Map<String, Object> index() {
-        String input1 = "password1";
-        String input2 = "password2";
-
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        byte[] hash1 = md.digest(input1.getBytes());
-        byte[] hash2 = md.digest(input2.getBytes());
-
-        System.out.println("Hash1: " + new String(hash1));
-        System.out.println("Hash2: " + new String(hash2));
+        String sql = "SELECT * FROM users WHERE username='" + username + "' AND password='" + password + "'";
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
         return Collections.singletonMap("message", "Hello World feature!");//as
 
     }
