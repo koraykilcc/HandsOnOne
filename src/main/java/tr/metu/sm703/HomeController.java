b84f94962e78f8118a32ea86003b27d703b237a8
Rahmi Kılıç <koray.kilic@metu.edu.tr>
2023-06-11 14:09:41 +0300

    dev change

--- src/main/java/tr/metu/sm703/HomeController.java
+++ src/main/java/tr/metu/sm703/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
     @Get
     public Map<String, Object> index() {
 
-        return Collections.singletonMap("message", "Hello world develop1!");
+        return Collections.singletonMap("message", "Hello world develop!");
 
 
     }
