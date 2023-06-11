6d52c27fe84edd6d28de5d04951706ebd8c1fa25
Rahmi Kılıç <koray.kilic@metu.edu.tr>
2023-06-11 16:33:25 +0300

    mast

--- src/main/java/tr/metu/sm703/HomeController.java
+++ src/main/java/tr/metu/sm703/HomeController.java
@@ -9,6 +9,7 @@ public class HomeController {
 
     @Get
     public Map<String, Object> index() {
+        String password ="asdasd";
         return Collections.singletonMap("message", "Hello World producti!");
     }
 }
