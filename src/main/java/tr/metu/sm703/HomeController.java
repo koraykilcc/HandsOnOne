2ce74f02e152554f8064db040b09ea6c283806ba
Rahmi Kılıç <koray.kilic@metu.edu.tr>
2023-06-11 16:49:29 +0300

    mas

--- src/main/java/tr/metu/sm703/HomeController.java
+++ src/main/java/tr/metu/sm703/HomeController.java
@@ -1,6 +1,8 @@
 package tr.metu.sm703;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+
+import java.sql.PreparedStatement;
 import java.util.Collections;
 import java.util.Map;
 
@@ -9,7 +11,13 @@ public class HomeController {
 
     @Get
     public Map<String, Object> index() {
-        String password ="asdasd";
+        String query = String.format("SELECT secret FROM Users WHERE (username = '%s' AND NOT role = 'admin')", username);
+// Execute query and return the results
+        // Get username from parameters
+        String username = request.getParameter("username");
+// Create a statement from database connection
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(query);
         return Collections.singletonMap("message", "Hello World producti!");
     }
 }
