e85ae7a99e1bf62f8b99a2f58c7cd721b3215101
Rahmi Kılıç <koray.kilic@metu.edu.tr>
2023-06-11 17:04:49 +0300

    mas

--- src/main/java/tr/metu/sm703/HomeController.java
+++ src/main/java/tr/metu/sm703/HomeController.java
@@ -12,11 +12,11 @@ public class HomeController {
 
     @Get
     public Map<String, Object> index() {
-        String PASSWORD="123444";
-        Scanner myObj = new Scanner(System.in);
-        String userId = myObj.nextLine();
+        //String PASSWORD="123444";
+        //Scanner myObj = new Scanner(System.in);
+        //String userId = myObj.nextLine();
 
-        String sqlQuery = "select * from tbluser where userId = " + userId;
+        //String sqlQuery = "select * from tbluser where userId = " + userId;
         return Collections.singletonMap("message", "Hello World producti!");
     }
 }
