620c6a2b47f007bbffd5ff5ec32445afc7cfb12e
Rahmi Kılıç <koray.kilic@metu.edu.tr>
2023-06-11 16:59:57 +0300

    mas

--- src/main/java/tr/metu/sm703/HomeController.java
+++ src/main/java/tr/metu/sm703/HomeController.java
@@ -5,6 +5,7 @@ import io.micronaut.http.annotation.Get;
 import java.sql.PreparedStatement;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Scanner;
 
 @Controller
 public class HomeController {
@@ -12,6 +13,10 @@ public class HomeController {
     @Get
     public Map<String, Object> index() {
         String PASSWORD="123444";
+        Scanner myObj = new Scanner(System.in);
+        String userId = myObj.nextLine();
+
+        String sqlQuery = "select * from tbluser where userId = " + userId;
         return Collections.singletonMap("message", "Hello World producti!");
     }
 }
