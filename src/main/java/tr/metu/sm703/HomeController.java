e9ae61e65f962516c7cb8f44dbcec42570f471af
Rahmi Kılıç <koray.kilic@metu.edu.tr>
2023-06-17 17:29:13 +0300

    feature3

--- src/main/java/tr/metu/sm703/HomeController.java
+++ src/main/java/tr/metu/sm703/HomeController.java
@@ -1,6 +1,10 @@
 package tr.metu.sm703;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 import java.util.Map;
 
@@ -8,8 +12,9 @@ import java.util.Map;
 public class HomeController {
 
     @Get
-    public Map<String, Object> index() {
+    public Map<String, Object> index() throws NoSuchPaddingException, NoSuchAlgorithmException {
         String password="password";
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         return Collections.singletonMap("message", "Hello World feature2!");
 
     }
